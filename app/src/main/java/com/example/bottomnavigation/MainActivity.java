41edb67833d9755447af4bedc8a3f35edaf1e69e
JuanTorresYah <juan.torres@cbtis72.edu.mx>
2019-05-16 20:46:34 -0500

    Se agregó la barra de navegación y se modificó el MainActivity

--- app/src/main/java/com/example/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -1,6 +1,15 @@
 package com.example.bottomnavigation;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,44 +18,54 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.bottomnavigation.fragment.CartFragment;
+import com.example.bottomnavigation.fragment.GiftsFragment;
+import com.example.bottomnavigation.fragment.ProfileFragment;
+import com.example.bottomnavigation.fragment.StoreFragment;
+import com.example.bottomnavigation.helper.BottomNavigationBehavior;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+         toolbar = getSupportActionBar();
+
+         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+         navigation.setOnNavigationItemReselectedListener(mOnNavigationItemSelectedListener);
+
+         toolbar.setTitle("Shop");
+    }
+
+    private BottomNavigationView.OnNavigationItemReselectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemReselectedListener() {
+        //Permite que el titulo de la barra de navegacion cambie dependiendo de el elemento seleccionado
+        @Override
+        public void onNavigationItemReselected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()){
+                case R.id.navigation_shop:
+                    toolbar.setTitle("Shop");
+                    return true;
+                case R.id.navigation_gifts:
+                    toolbar.setTitle("My Gifts");
+                    return true;
+                case R.id.navigation_cart:
+                    toolbar.setTitle("Cart");
+                    return true;
+                case R.id.navigation_profile:
+                    toolbar.setTitle("Profile");
+                    return true;
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            return false;
         }
+    };
 
-        return super.onOptionsItemSelected(item);
-    }
 }
